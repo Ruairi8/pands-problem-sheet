e03509dfc70d41edb73ab7c3387a734c019a8cc6
Ruairi <G00411334@gmail.ie>
2022-02-01 23:12:08 +0000

    bmi.py

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pands-problem-sheet
+
+# Week 2 problem question
+
+# Write a program to calculate a person's body mass index (BMI)
---
+++ bmi.py
@@ -0,0 +1,6 @@
+# My solution to the question from week 2
+# Author: Ruairi McCool
+
+height = input("Enter height in cm: ")
+weight = input("Enter weight in kg: ")
+bmi = (weight /(height/100)**2)
