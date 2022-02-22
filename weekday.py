b9c1625675e8fb1aab87d86d814d7433044970cf
Ruairi <G00411334@gmail.ie>
2022-02-22 23:24:36 +0000

    weekday

--- weekday.py
+++ weekday.py
@@ -1,10 +1,11 @@
 # A program that outputs whether or not today is a weekday.
 # Author: Ruairi McCool
 
-import datetime
-x = ''
-if user = input(x.weekday()):
-    x.append(user)
-    print("Yes, unfortunately today is a {}".format(user))
-esle:
-    print("It is the weekend, yay!")
+
+from datetime import datetime
+print(datetime.today())
+
+result = datetime.today().weekday()
+print(result)
+
+
